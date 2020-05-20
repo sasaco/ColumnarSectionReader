36e56968d75d81f05db867c919ae04c058bb1209
sasaco <sasaicco@gmail.com>
2020-05-20 22:44:11 +0900

    post 処理

--- README.md
+++ README.md
@@ -1,27 +1,8 @@
-# ColumnarSectionReader
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
+## 参考サイト
 
-## Development server
+### Angular (javascript, html) に関して
+https://medium.com/@dancornilov/angular-5-image-uploader-component-api-integration-69dadf3e2728
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+### Google Cloud Vision について
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ColumnarSectionReader';
+
+  selectedFile: File = null;
+  @Output() event = new EventEmitter();
+  public image: any;
+
+  private url: string = 'https://vision.googleapis.com/v1/images:annotate?key=' +
+    'AIzaSyBGsWzGU2G9sxIRRRAalNajUSBSyASBCQM' //Your API Key
+  
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   *  Validate uploader params
+   * @param event
+   */
+  public async onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+    if (this.selectedFile) {
+       const fd = new FormData();
+      fd.append('image', this.selectedFile, this.selectedFile.name);
+      return this.upload(fd);
+      
+    }
+  }
+
+  /**
+   * Upload image to server and receive image object
+   */
+  private upload(fd: FormData) {
+
+    this.http.post(this.url, fd).subscribe((res: any) => {
+      this.image = res.data;
+      this.event.emit(this.image);
+    }, (err: any) => {
+      // Show error message or make something.
+    });
+  }
+
 }
