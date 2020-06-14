a82267249defbc1652acef89376dd235f04c24e9
sasaco <sasaicco@gmail.com>
2020-06-14 23:05:06 +0900

    Cloud Vision の結果から 柱状図に枠線を付けた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, ViewChild, Output, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -6,19 +6,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
+  @ViewChild('myCanvas', { static: false }) myCanvas;
   @Output() event = new EventEmitter();
 
   public image: any;
+  public canvasWidth: number;
+  public canvasHeight: number;
+  private img: any;
 
   private url: string = 'https://vision.googleapis.com/v1/images:annotate?key=' +
-    'Your API Key';
+    'AIzaSyC4o30DMu1pnWjoJPMr0jqyzXQrM3U8DmU'; //Your API Key
 
-  
+
   constructor(private http: HttpClient) {
   }
 
+  context: CanvasRenderingContext2D;
+
+  ngAfterViewInit() {
+    // 参照をとれる
+    const canvas = this.myCanvas.nativeElement;
+    this.context = canvas.getContext('2d');
+  }
 
   public onFileSelected(event) {
 
@@ -31,7 +42,12 @@ export class AppComponent {
       reader.readAsDataURL(selectedFile);
       reader.onload = (_event) => {
         this.image = reader.result;
-
+        this.img = new Image();
+        this.img.src = this.image;
+        this.img.onload = () => {
+          this.canvasWidth = this.img.width;
+          this.canvasHeight = this.img.height;
+        }
         // Google Cloud Vision API に送る
         return this.upload();
       }
@@ -66,19 +82,45 @@ export class AppComponent {
         'Content-Type': 'application/json'
       })
     }).subscribe((res: any) => {
-      this.event.emit(this.image);
       // Google Cloud Vision API からのレスポンスを表示する
-      alert("成功！！ console.log に画像認識 した結果を出力しました。");
-      console.log(res);
+      alert("成功！！ 画像に アノテーションを追加しました。");
+      this.draw(res);
+
     }, (err: any) => {
       // エラー
-        alert("エラー console.log を確認してください。");
+      alert("エラー console.log を確認してください。");
       console.log(err);
     });
 
+  }
 
+  // Canvas要素を更新します。 
+  private draw(res: any): void {
+    const ctx = this.context;
+    if (!ctx) {
+      return;
+    }
+    // 画像の読み込み
+    ctx.drawImage(this.img, 0, 0);
+    
+    // 矩形を描く
+    ctx.strokeStyle = '#FF0000';
 
+    const responses = res.responses[0];
+    const textAnnotations = responses.textAnnotations;
+    for (let i = 1; i < textAnnotations.length; i++) {
+      
+      const block = textAnnotations[i];
+      const text: string = block.description;
+      const pos: Array<any> = block.boundingPoly['vertices'];
 
+      const w: number = pos[2].x - pos[0].x;
+      const h: number = pos[2].y - pos[0].y;
+      ctx.strokeRect(pos[0].x, pos[0].y, w, h);
+
+    }
+
+    console.log(res);
   }
 
 }
